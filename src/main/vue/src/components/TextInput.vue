b216d0f8481bb27f1d88b6bd978617ef57868db6
sashapleshyova <sasha.pleshyova@yandex.ru>
2019-02-28 22:45:25 +0400

    added component TextInput

---
+++ src/main/vue/src/components/TextInput.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="input-form">
+        <label class="input-label" for="input-field">{{label}}</label>
+        <input id="input-field" class="input-field" :placeholder="placeholder" :type="type"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TextInput",
+        props: {
+            label: {
+                required: false
+            },
+            placeholder: {
+                required: false
+            },
+            value: {},
+            type: {
+                type: String,
+                default: "text"
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .input-form {
+        display: flex;
+        flex-direction: column;
+    }
+    .input-label{
+        width: max-content;
+        color: white;
+        margin-bottom: 7px;
+        font-size: 16px;
+    }
+    .input-field {
+        outline: none;
+        border: 1px solid #0C0094;
+        width: 300px;
+        padding: 10px;
+        font-size: 15px;
+        border-radius: 4px;
+        box-shadow:1px 2px 12px 0 rgba(0,0,0,0.3);
+    }
+</style>
