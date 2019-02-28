49afb01370eadda157bd4684e13cfc6d75379987
sashapleshyova <sasha.pleshyova@yandex.ru>
2019-03-01 00:07:20 +0400

    added authorization form

---
+++ src/main/vue/src/views/AuthorizationForm.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="content">
+        <div class="container">
+            <TextInput class="field" placeholder="E-mail или логин" type="text"/>
+            <TextInput class="field" placeholder="Пароль" type="password"/>
+            <Button class="forget-password" label="Забыли пароль?" type="light"/>
+            <Button class="entry" label="Вход"/>
+            <Button class="register" label="Зарегистрироваться" type="light"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TextInput from "../components/TextInput";
+    import Button from "../components/Button";
+
+    export default {
+        name: "AuthorizationForm",
+        components: {Button, TextInput}
+    }
+</script>
+
+<style scoped>
+    .content {
+        height: 100vh;
+        background: slateblue; /*временно, пока нет фоновой картинки*/
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .field:nth-child(1) {
+        margin-bottom: 20px
+    }
+
+    .field:nth-child(2) {
+        margin-bottom: 10px
+    }
+
+    .forget-password {
+        width: 100%;
+
+        text-align: end;
+    }
+
+    .entry {
+        margin-top: 20px;
+    }
+
+    .register {
+        margin-top: 20px;
+    }
+</style>
