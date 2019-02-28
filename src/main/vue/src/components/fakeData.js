f79de7370d6a75bde21bc6e88a20120d772e71cc
Nail Yusupov <44222214+NailJusupov@users.noreply.github.com>
2019-02-28 23:33:10 +0400

    16 feature main page (#20)
    * Connected fonts to project. Added global styles to App.vue
    
    * Made test page. Added playlist element component
    
    * Finished playlist element component
    
    * Made find playlist component
    
    * Made party sound label component
    
    * Made top playlist component
    
    * Made main page
    
    * Added settings button

---
+++ src/main/vue/src/components/fakeData.js
@@ -0,0 +1,182 @@
+export const fakeData = [
+    {
+        playlistId: 0,
+        playlistName: 'Название',
+        color: '#D96DFF',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 1,
+        playlistName: 'Название',
+        color: '#FFF500',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 2,
+        playlistName: 'Название',
+        color: '#FF7F23',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 3,
+        playlistName: 'Название',
+        color: '#D96DFF',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 4,
+        playlistName: 'Название',
+        color: '#C1C1C1',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 5,
+        playlistName: 'Название',
+        color: '#D96DFF',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 6,
+        playlistName: 'Название',
+        color: '#FFF500',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 7,
+        playlistName: 'Название',
+        color: '#FF7F23',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 8,
+        playlistName: 'Название',
+        color: '#D96DFF',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 9,
+        playlistName: 'Название',
+        color: '#C1C1C1',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 10,
+        playlistName: 'Название',
+        color: '#D96DFF',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 11,
+        playlistName: 'Название',
+        color: '#FFF500',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 12,
+        playlistName: 'Название',
+        color: '#FF7F23',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 13,
+        playlistName: 'Название',
+        color: '#D96DFF',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    },
+    {
+        playlistId: 14,
+        playlistName: 'Название',
+        color: '#C1C1C1',
+        genres: [
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр',
+            'жанр'
+        ]
+    }
+];
