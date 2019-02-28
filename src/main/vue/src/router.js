f0615017529d04c497053b0c194e22f7be80d26f
sashapleshyova <sasha.pleshyova@yandex.ru>
2019-03-01 01:28:55 +0400

    added registration form

--- src/main/vue/src/router.js
+++ src/main/vue/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Main from './views/Main.vue';
+import RegistrationForm from './views/RegistrationForm'
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
             path: '/',
             name: 'main',
             component: Main
+        },
+        {
+            path: '/registration',
+            name: 'registrationForm',
+            component: RegistrationForm
         }
     ]
 });
---
+++ src/main/vue/src/views/RegistrationForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <main class="content">
+        <div class="container">
+            <TextInput class="child" placeholder="E-mail"/>
+            <TextInput class="child" placeholder="Логин"/>
+            <TextInput class="child" placeholder="Имя"/>
+            <TextInput class="child" placeholder="Пароль" type="password"/>
+            <TextInput class="child" placeholder="Введите пароль еще раз" type="password"/>
+            <Button class="child" label="Зарегистрироваться"/>
+            <Button class="child" label="Войти" type="light"/>
+        </div>
+    </main>
+</template>
+
+<script>
+    import TextInput from "../components/TextInput";
+    import Button from "../components/Button";
+
+    export default {
+        name: "RegistrationForm",
+        components: {Button, TextInput}
+    }
+</script>
+
+<style scoped>
+    .content {
+        height: 100vh;
+        background: slateblue;  /*временно, пока нет фоновой картинки*/
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .child{
+        margin-bottom: 20px;
+    }
+    .child:nth-child(5){
+        margin-bottom: 30px;
+    }
+</style>
