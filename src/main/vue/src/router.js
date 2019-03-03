9b00494f213eac6da364dec3baf104c194818b62
Aleksandra Plescheva <46083995+sashaplescheva@users.noreply.github.com>
2019-03-03 20:38:34 +0400

    8 feature authorization form (#23)
    * Connected fonts to project. Added global styles to App.vue
    
    * added component button
    
    * added component TextInput
    
    * added authorization form
    
    * added authorization form
    
    * changes to the authorization form

--- src/main/vue/src/router.js
+++ src/main/vue/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Main from './views/Main.vue';
+import AuthorizationForm from './views/AuthorizationForm'
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
             path: '/',
             name: 'main',
             component: Main
-        }
+        },
+        {
+            path: '/authorization',
+            name: 'authorizationForm',
+            component: AuthorizationForm
+        },
     ]
 });
