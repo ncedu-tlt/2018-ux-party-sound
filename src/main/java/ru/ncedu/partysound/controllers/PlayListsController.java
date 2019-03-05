659851d0ca9bb81c94102470e0753eabe028226b
Evgenii <id18780@yandex.ru>
2019-03-06 01:58:33 +0400

    create controller.

---
+++ src/main/java/ru/ncedu/partysound/controllers/PlayListsController.java
@@ -0,0 +1,27 @@
+package ru.ncedu.partysound.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.ncedu.partysound.models.domain.PlaylistsDAO;
+import ru.ncedu.partysound.repositories.PlaylistsRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/play_lists")
+public class PlayListsController {
+
+    private final PlaylistsRepository playlistsRepository;
+
+    public PlayListsController(PlaylistsRepository playlistsRepository) {
+        this.playlistsRepository = playlistsRepository;
+    }
+
+    @GetMapping
+    public List<PlaylistsDAO> get() {
+        return playlistsRepository.findAll();
+    }
+}
