4abc8f64152c09b08182ee35d9ee780ada7a8a2e
Evgenii <id18780@yandex.ru>
2019-03-20 16:26:39 +0400

    Fix code.

--- src/main/java/ru/ncedu/partysound/controllers/TracksController.java
+++ src/main/java/ru/ncedu/partysound/controllers/TracksController.java
@@ -1,8 +1,12 @@
 package ru.ncedu.partysound.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import ru.ncedu.partysound.models.domain.GenresDAO;
 import ru.ncedu.partysound.models.domain.TracksDAO;
+import ru.ncedu.partysound.repositories.GenresRepository;
 import ru.ncedu.partysound.repositories.TracksRepository;
+
+import java.util.List;
 
 
 @RestController
@@ -10,14 +14,18 @@ import ru.ncedu.partysound.repositories.TracksRepository;
 public class TracksController {
 
     private final TracksRepository tracksRepository;
+    private final GenresRepository genresRepository;
 
-    public TracksController(TracksRepository tracksRepository) {
+    public TracksController(TracksRepository tracksRepository, GenresRepository genresRepository) {
         this.tracksRepository = tracksRepository;
+        this.genresRepository = genresRepository;
     }
 
     @PostMapping
     public String post(@RequestBody TracksDAO track) {
         tracksRepository.save(track);
+        List<String> genres = track.getGenres();
+        List<GenresDAO> genresDAOS = genresRepository.findAllByName();
         return "Трек успешно сохранен с id" + track.getId();
     }
 }
