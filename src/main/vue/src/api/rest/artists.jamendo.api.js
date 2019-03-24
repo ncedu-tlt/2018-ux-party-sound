b638016d1e788cffab81a892fb38feed80b8e2b9
Evgenii <id18780@yandex.ru>
2019-03-24 18:44:11 +0400

    fix code for description.

--- src/main/vue/src/api/rest/artists.jamendo.api.js
+++ src/main/vue/src/api/rest/artists.jamendo.api.js
@@ -1,8 +1,9 @@
 import jamendoClient from '@/api/rest/jamendo.client';
 
 /**
- * @param { String } namesearch задает подстроку для поиска
- * @param { Number } limit задает количество исполнителей
+ * @param { Object } params задает подстроку для поиска
+ * @param { String } params.namesearch задает подстроку для поиска
+ * @param { Number } params.limit задает количество исполнителей
  */
 export function findArtistsByName({ limit, namesearch }) {
     return jamendoClient.get('/artists', { params: arguments[0] })
