6aaae08169827e5e243f67cddeffd9593862c219
KhMaria <hvostova_masha@mail.ru>
2019-04-04 21:12:30 +0400

    Change request to database

--- src/main/java/ru/ncedu/partysound/repositories/PlaylistsRepository.java
+++ src/main/java/ru/ncedu/partysound/repositories/PlaylistsRepository.java
@@ -17,9 +17,9 @@ public interface PlaylistsRepository extends PagingAndSortingRepository<Playlist
             "FROM (SELECT user_playlist.playlist_id, COUNT(*) AS roleCount\n" +
             "FROM user_playlist INNER JOIN roles  ON user_playlist.role_id=roles.id\n" +
             "INNER JOIN playlists ON user_playlist.playlist_id=playlists.id\n" +
-            "WHERE roles.name=?1 AND playlists.private_access=false\n" +
+            "WHERE roles.name='listener' AND playlists.private_access=false\n" +
             "GROUP BY user_playlist.playlist_id) plrole\n" +
             "ORDER BY plrole.roleCount desc\n" +
             "limit 5", nativeQuery = true)
-    List<BigInteger> getTopPlaylistsId(String roleName);
+    List<BigInteger> getTopPlaylistsId();
 }
